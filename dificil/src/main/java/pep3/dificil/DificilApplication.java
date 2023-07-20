0c8d9a23fbfc55d287cad2afeb583e99ed43709d
RicardoUsachita <ricardo.arancibia@usach.cl>
2023-07-20 02:01:44 -0400

    nuevas entidades

--- dificil/src/main/java/pep3/dificil/DificilApplication.java
+++ dificil/src/main/java/pep3/dificil/DificilApplication.java
@@ -2,8 +2,10 @@ package pep3.dificil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DificilApplication {
 
 	public static void main(String[] args) {
---
+++ dificil/src/main/java/pep3/dificil/clases/PreguntaE.java
@@ -0,0 +1,21 @@
+package pep3.dificil.clases;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "preguntas-dificil")
+@NoArgsConstructor
+@AllArgsConstructor
+public class PreguntaE {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String pregunta;
+    private String codigo;
+    private String respuesta;
+}
